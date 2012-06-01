8807ddd7a005505f6036f3d0a2c4f8c67c5df33a
isimilej <isimilej@gmail.com>
2012-06-01 17:26:31 +0900

    그룹 추가 폼 정리

--- HerbFamily/src/com/herbfamily/NewGroupActivity.java
+++ HerbFamily/src/com/herbfamily/NewGroupActivity.java
@@ -5,23 +5,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class NewGroupActivity extends Activity {
+public class NewGroupActivity extends Activity implements View.OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_group);
 		
-		Button buttonAddGroup = (Button)findViewById(R.id.buttonAddNewGroup);
-		buttonAddGroup.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-			}
-		});
-		
-		Button buttonGroupCancel = (Button)findViewById(R.id.buttonCancelGroup);
-		buttonGroupCancel.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				finish();
-			}
-		});
+		((Button)findViewById(R.id.buttonAddNewGroup)).setOnClickListener(this);
+		((Button)findViewById(R.id.buttonCancelGroup)).setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.buttonAddNewGroup:
+			break;
+		case R.id.buttonCancelGroup:
+			finish();
+			break;
+		}
 	}
 }
