0ca6572f26411247695cda7ecabc25a8cffacb50
senku <senku7@gmail.com>
2012-06-14 05:05:30 +0900

    <전화&문자 > 대상에게 연결은 안됨

--- HerbFamily/src/com/herbfamily/HerbFamilyActivity.java
+++ HerbFamily/src/com/herbfamily/HerbFamilyActivity.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,26 +14,29 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class HerbFamilyActivity extends ListActivity implements View.OnClickListener {
-	
+public class HerbFamilyActivity extends ListActivity implements
+		View.OnClickListener {
+
 	private MemberListAdapter adapter;
 	private ArrayList<Member> members;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
 
-		//TODO onResume 에서 계속해서 DB 를 읽게 되네, 별로 좋치 않다. - NewForm 에서 activity return 값을 reflash 해야 겠다.
+		// TODO onResume 에서 계속해서 DB 를 읽게 되네, 별로 좋치 않다. - NewForm 에서 activity
+		// return 값을 reflash 해야 겠다.
 		MemberDatabase database = new MemberDatabase(this);
 		members = database.getMembers();
+
 		database.close();
-		
+
 		setupWidgets();
 	}
 
@@ -44,11 +48,13 @@ public class HerbFamilyActivity extends ListActivity implements View.OnClickList
 	private void setupWidgets() {
 		adapter = new MemberListAdapter(this);
 		setListAdapter(adapter);
-		
-		((Button)findViewById(R.id.buttonOpenMemberAddition)).setOnClickListener(this);
-		((Button)findViewById(R.id.buttonOpenGroupAddidtion)).setOnClickListener(this);
+
+		((Button) findViewById(R.id.buttonOpenMemberAddition))
+				.setOnClickListener(this);
+		((Button) findViewById(R.id.buttonOpenGroupAddidtion))
+				.setOnClickListener(this);
 	}
-	
+
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.buttonOpenMemberAddition:
@@ -59,7 +65,7 @@ public class HerbFamilyActivity extends ListActivity implements View.OnClickList
 			break;
 		}
 	}
-	
+
 	private class MemberListAdapter extends BaseAdapter {
 		private Context context;
 
@@ -70,7 +76,7 @@ public class HerbFamilyActivity extends ListActivity implements View.OnClickList
 		public int getCount() {
 			return members.size();
 		}
-		
+
 		public Object getItem(int position) {
 			return position;
 		}
@@ -84,24 +90,48 @@ public class HerbFamilyActivity extends ListActivity implements View.OnClickList
 			if (convertView == null) {
 				LayoutInflater inflater = LayoutInflater.from(context);
 				convertView = inflater.inflate(R.layout.member_list_row, null);
-				
+
 				holder = new ViewHolder();
-				holder.text = (TextView)convertView.findViewById(R.id.memberName);
+				holder.text = (TextView) convertView.findViewById(R.id.memberName);
+				holder.btn1 = (Button) convertView.findViewById(R.id.buttonCall);
+				holder.btn2 = (Button) convertView.findViewById(R.id.buttonSMS);
 				convertView.setTag(holder);
+				
+				holder.btn1.setOnClickListener(new Button.OnClickListener(){
+					public void onClick(View v) {
+							Intent call = new Intent(Intent.ACTION_CALL);
+							call.setData(Uri.parse("tel:01076487749"));
+							startActivity(call);
+						}
+					});
+				
+				holder.btn2.setOnClickListener(new Button.OnClickListener(){
+					public void onClick(View v) {
+						Uri smsUri = Uri.parse("tel:01076487749");  
+						Intent intent = new Intent(Intent.ACTION_VIEW, smsUri);  
+						intent.putExtra("sms_body", "아놔 ~~  네 번호가 왜 안찍히냐고!!!!!!!!!!!");  
+						intent.setType("vnd.android-dir/mms-sms");   
+						startActivity(intent); 
+						}
+
+					});
+
 			} else {
-				holder = (ViewHolder)convertView.getTag();
+				holder = (ViewHolder) convertView.getTag();
 			}
 			
-			Member member = (Member)members.get(position);
-			holder.text.setText(member.getName() + "(" + member.getNickname() + ")");
+
+			Member member = (Member) members.get(position);
+			holder.text.setText(member.getName() + "(" + member.getNickname()
+					+ ")");
 			
 			return convertView;
 		}
-		
+
 		private class ViewHolder {
 			private TextView text;
+			private Button btn1, btn2;
 		}
 	}
 
-	
 }
