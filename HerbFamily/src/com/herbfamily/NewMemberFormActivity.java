a9048b59c37a69f42c2f33d630aef0e95ab85fc6
senku <senku7@gmail.com>
2012-06-14 14:44:22 +0900

    Dialog 구동만 (시간 X) / 연결 X

--- HerbFamily/src/com/herbfamily/NewMemberFormActivity.java
+++ HerbFamily/src/com/herbfamily/NewMemberFormActivity.java
@@ -1,7 +1,11 @@
 package com.herbfamily;
 
+import java.util.Calendar;
+
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +14,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class NewMemberFormActivity extends Activity implements View.OnClickListener {
 	
@@ -17,7 +23,7 @@ public class NewMemberFormActivity extends Activity implements View.OnClickListe
 	private Contact contact = null;
 	
 	private ImageButton imgBtn;
-	private Button buttonOpenGroupChoice;
+	private Button buttonOpenGroupChoice, cycleBtn, timeBtn, alarmTypeBtn, alarmToneBtn;
 	private static final String TAG = NewMemberFormActivity.class.getSimpleName();
 	private int groupId = 0;
 
@@ -86,24 +92,80 @@ public class NewMemberFormActivity extends Activity implements View.OnClickListe
 				finish();
 			}
 		});
-		
+		cycleBtn = (Button)findViewById(R.id.cycleBtn);
+		cycleBtn.setOnClickListener(this);
+		timeBtn = (Button)findViewById(R.id.timeBtn);
+		timeBtn.setOnClickListener(this);
+		alarmTypeBtn = (Button)findViewById(R.id.alarmTypeBtn);
+		alarmTypeBtn.setOnClickListener(this);
+		alarmToneBtn = (Button)findViewById(R.id.alarmToneBtn);
+		alarmToneBtn.setOnClickListener(this);
 		buttonOpenGroupChoice = (Button)findViewById(R.id.buttonOpenGroupChoice);
 		buttonOpenGroupChoice.setOnClickListener(this);
 		
 		imgBtn = (ImageButton)findViewById(R.id.choice);
-		imgBtn.setBackgroundResource(R.drawable.ic_launcher);
+//		imgBtn.setBackgroundResource(R.drawable.ic_launcher);
         imgBtn.setOnClickListener(this);
 	}
 	
 	public void onClick(View view) {
 		switch (view.getId()) {
+		case R.id.choice:
+    		Intent herb = new Intent(NewMemberFormActivity.this, Dialog.class);
+			startActivityForResult(herb, Consts.DIALOG_REQUEST_CODE);
+			break;
+			
 		case R.id.buttonOpenGroupChoice:
 			Intent intent = new Intent(NewMemberFormActivity.this, GroupChoiceActivity.class);
 			startActivityForResult(intent, Consts.GROUP_REQUEST_CODE);
 			break;
-		case R.id.choice:
-    		Intent herb = new Intent(NewMemberFormActivity.this, Dialog.class);
-			startActivityForResult(herb, 10);
+			
+		case R.id.cycleBtn:
+			final String[] cycle = {"3일", "5일", "7일", "10일"};
+			
+			new AlertDialog.Builder(NewMemberFormActivity.this)
+			.setTitle("알람주기")
+			.setSingleChoiceItems(cycle, 0, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog ,int which) {
+					Toast.makeText(NewMemberFormActivity.this, "cycle : " + cycle[which],Toast.LENGTH_SHORT).show();
+					dialog.dismiss();
+				}
+			}).setNeutralButton("닫기", null).show();
+	
+			break;
+
+		case R.id.timeBtn:
+
+			break;
+			
+		case R.id.alarmTypeBtn:
+			final String[] type = {"벨소리", "진동", "진동 및 벨소리", "무음"};
+			
+			new AlertDialog.Builder(NewMemberFormActivity.this)
+			.setTitle("알람방식")
+			.setSingleChoiceItems(type, 0, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog ,int which) {
+					Toast.makeText(NewMemberFormActivity.this, "type : " + type[which],Toast.LENGTH_SHORT).show();
+					dialog.dismiss();
+				}
+			}).setNeutralButton("닫기", null).show();
+	
+			break;
+			
+		case R.id.alarmToneBtn:
+			final String[] tone = {"1번 벨소리", "2번 벨소리", "3번 벨소리", "4번 벨소리"};
+			
+			new AlertDialog.Builder(NewMemberFormActivity.this)
+			.setTitle("알람음")
+			.setSingleChoiceItems(tone, 0, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog ,int which) {
+					Toast.makeText(NewMemberFormActivity.this, "tone : " + tone[which],Toast.LENGTH_SHORT).show();
+					dialog.dismiss();
+				}
+			}).setNeutralButton("닫기", null).show();
+	
+			break;
+			
 		}
 	}
 	
